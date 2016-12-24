39faff6c34ecb63145dbcecf149cb2292022d35d
noven temer <noven5823@gmail.com>
2016-12-24 17:46:56 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,32 @@
+def reverse_each_word(str)
+  new_str=""
+  arr=str.split(" ")
+  arr.each do |e|
+    w=e.reverse
+    if new_str == ""
+       new_str=new_str + "" + w
+     else
+        new_str=new_str + " " + w
+      end
+      end
+  return new_str
+end
+
+def reverse_each_word(str)
+  new_str=""
+  arr=str.split(" ")
+  arr.collect do |e|
+    w=e.reverse
+    if new_str == ""
+       new_str=new_str + "" + w
+     else
+        new_str=new_str + " " + w
+      end
+    end
+
+  return new_str
+end
+
+puts reverse_each_word("Hello there, and how are you?")
+# olleH ,ereht dna woh era ?uoy
+puts reverse_each_word("Hi again, just making sure it's reversed!")
